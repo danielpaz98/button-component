ce418a2b0549c1f8ee3cb94f329ca633e511e065
danielpaz98 <alexdaniel-1998@outlook.com>
2021-08-11 18:30:56 -0500

    Add button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+import "~/assets/css/button.css";
+// PLUGINS
+import classNames from "clsx";
+
+export default function Button(props) {
+	const { text = "Click me", color, variant, size, icon, ...staticStates } = props;
+	const { disabled } = staticStates;
+
+	const classNameBtn = classNames("btn", {
+		[`btn-${color}`]: color && !disabled,
+		[`btn-${variant}`]: variant,
+		[`size-${size}`]: size && size != "default",
+		[`icon-${icon}`]: icon,
+		...staticStates,
+	});
+
+	const classNameBtnIcon = classNames("material-icons", { show: Boolean(icon) });
+
+	return (
+		<button type="button" className={classNameBtn} disabled={disabled}>
+			<span>{text}</span>
+			<i className={classNameBtnIcon}>{icon}</i>
+		</button>
+	);
+}
+
+Button.displayName = "Button";
