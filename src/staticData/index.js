119c8adf68b8106c7e11eed3e4816378926387cf
danielpaz98 <alexdaniel-1998@outlook.com>
2021-08-11 18:34:39 -0500

    Add static data for the different options for the button state

---
+++ src/staticData/index.js
@@ -0,0 +1,26 @@
+export const checkboxOptions = [
+	{ text: "Block", value: "block" },
+	{ text: "Disabled", value: "disabled" },
+	{ text: "Disabled box shadow", value: "disabled-box-shadow" },
+];
+
+export const iconOptions = [
+	{ text: "shoping cart", value: "shopping_cart" },
+	{ text: "search", value: "search" },
+	{ text: "favorite", value: "favorite" },
+	{ text: "thumb up", value: "thumb_up" },
+	{ text: "done outline", value: "done_outline" },
+];
+
+export const colorOptions = [
+	{ text: "primary", value: "primary" },
+	{ text: "secondary", value: "secondary" },
+	{ text: "danger", value: "danger" },
+];
+
+export const variantOptions = [
+	{ text: "outline", value: "outline" },
+	{ text: "text", value: "text" },
+];
+
+export const sizeOptions = ["default", "small", "medium", "large"];
