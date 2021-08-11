8dec371c3d58d2161fad211e3ebae346910fc22d
danielpaz98 <alexdaniel-1998@outlook.com>
2021-08-11 18:32:04 -0500

    Create CheckboxList.jsx

---
+++ src/components/CheckboxList.jsx
@@ -0,0 +1,18 @@
+// PLUGINS
+import PropTypes from "prop-types";
+// COMPONENTS
+import Checkbox from "~/components/Checkbox";
+
+export default function CheckboxList({ values = [], onChange = () => {} }) {
+	return (
+		<div className="checkbox-list">
+			{values.map(({ text, value }, index) => (
+				<Checkbox key={index} labelText={text} value={value} onChange={onChange} />
+			))}
+		</div>
+	);
+}
+
+CheckboxList.propTypes = {
+	values: PropTypes.array.isRequired,
+};
