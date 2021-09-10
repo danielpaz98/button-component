61775f9dd6ae8795ee30c576b43a593ac376257b
danielpaz98 <alexdaniel-1998@outlook.com>
2021-09-10 18:52:05 -0500

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "~/assets/css/footer.css";
+
+export default function Footer() {
+	return (
+		<div className="main-footer">
+			<span>created by</span>
+
+			<a href="https://github.com/danielpaz98" target="_blank">
+				danielpaz98 -
+			</a>
+
+			<a href="https://devchallenges.io" target="_blank">
+				devChallenges.io
+			</a>
+		</div>
+	);
+}
